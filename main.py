4f9e5372a87fcab55989263daf77095e8601cc59
Shubham gupta Ggps <24877361+gptshubham595@users.noreply.github.com>
2021-07-15 19:50:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,27 +5,39 @@ Created on Thu Jul 15 02:15:17 2021
 @author: SHUBHAM
 """
 
-target="flipkart.in"
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 15 02:15:17 2021
 
+@author: SHUBHAM
+"""
+
+target="flipkart.com"
+
+#TOPOLOGY
 from webb import webb
 print(webb.traceroute(target,"flipkart_topology.txt"))
 webb.ping(target)
-webb.get_ip(target)
-
+#webb.get_ip(target)
 
 
 import whois
-w = whois.whois('flipkart.in')
+w = whois.whois('flipkart.com')
 w.expiration_date
 w.text
 
-import sublist3r 
-subdomains = sublist3r.main('flipkart.in', 40, 'subdomains.txt', ports= None,
-                            silent=False, verbose= False, enable_bruteforce= False,
-                            engines=None)
+import subprocess
+output = subprocess.Popen("ping flipkart.com", shell=True, stdout=subprocess.PIPE).stdout.read()
+print(output)
 
 
-#BUCKETS
+#SUBDOMAIN
+subprocess.Popen("python subscraper.py -e 3  flipkart.com", shell=True, stdout=subprocess.PIPE).stdout.read()
+
+subprocess.Popen("amass enum -passive -d flipkart.com -src", shell=True, stdout=subprocess.PIPE).stdout.read()
+
+
+#AWS BUCKETS
 import requests
 url="https://buckets.grayhatwarfare.com/api/v1/files/flipkart.in?access_token=e5289aeee79cbc1b9d4a312bd0cb6e38"
 resp = requests.get(url)
